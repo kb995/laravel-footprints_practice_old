2b423a4ec885eef726c1a57ecce4d5ab8ad19de5
kb995 <t.kuboshima995@gmail.com>
2020-09-09 00:45:39 +0900

    [add]リクエスト追加

--- app/Http/Requests/EditLog.php
+++ app/Http/Requests/EditLog.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateLog extends FormRequest
+class EditLog extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
